f93ee9aab584398022f4a8adf255a9768746d016
sb <jdzcn@qq.com>
2021-03-09 22:38:52 +0800

    index.php card style img

--- view.php
+++ view.php
@@ -10,21 +10,37 @@
 	$r = mysqli_fetch_assoc(mysqli_query($conn,$sql));
 
 	$img=explode('|',$r["images"]);
+?>
+	<main>
+		<aside>
+			<a href='images/<?=$img[0]?>'><img src='thumbnail/<?=$img[0]?>'></a>
+	<div class='bottomright'>
 
-	echo "<main><aside><a href='images/".$img[0]."'><img src='thumbnail/".$img[0]."' width=360 height=360></a></aside><article><div style='margin:10px;'>";
-	echo "<h2>".$r['name']."</h2>";
-	echo "<h4>编号：".$r['id']."</h4>";
-	echo "<h4>规格：".$r['spec']."</h4>";
-	echo "<h3 style='color:red'>￥".$r['price']."</h3></div></article></main>";
+	<ul class="pagination">
+	  <li><a href="#">❮</a></li>
+	  <li><a href="#">❯</a></li>
+	</ul>
+	</div>
+	</a></aside><article><div style='margin:10px;'>
+	<h3><?=$r['name']?></h3>
+	<h4>编号：<?=$r['id']?></h4>
+	<h4>规格：<?=$r['spec']?></h4>
+	<h3 style='color:red'>￥<?=$r['price']?></h3>
+	</div>
+	</article>
+	
+	</main>
 
-	echo "<div class='row'>";
-	echo "<h2>商品简介</h2>";
+	<div class='row'>
+	<h3>商品简介</h3>
+
+<?php
 	$tags=explode(',',$r['tags']);
 
 	foreach ($tags as $tag) {
 		$sql="select * from tag where id=".$tag;
 		$r = mysqli_fetch_assoc(mysqli_query($conn,$sql));
-		echo "<h3>".$r['name']."</h3>";
+		echo "<h4>".$r['name']."</h4>";
 		echo "<p>".$r['description']."</p>";
 	}
 	echo "</div>";
